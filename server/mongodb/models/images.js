a7086ba9d037c90c6111dba159f58b27e8250271
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-11-25 23:02:32 +0530

    Established MongoDB Connection and Created Image Collection Storing Schema

---
+++ server/mongodb/models/images.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const Image = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  prompt: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+});
+
+const ImageSchema = mongoose.model("Image", Image);
+export default ImageSchema;
