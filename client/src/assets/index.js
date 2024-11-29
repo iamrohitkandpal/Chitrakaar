8ea74464c4967097d6b3d1fe1ac4258ebc8c1944
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-11-30 01:58:13 +0530

    Some more styling and text changes

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,13 @@
 import download from './download.png';
 import logo from './logo.svg';
+import logop from './logop.svg';
+import logopi from './logopi.png';
 import preview from './preview.png';
 
 export {
   download,
   logo,
+  logop,
+  logopi,
   preview,
 };
